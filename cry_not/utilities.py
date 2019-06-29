02257ca30f93cf1384bae24514fae40eef7d11b5
Uzair Ali <uzaira960@gmail.com>
2019-06-29 15:15:00 +0100

    Added functionality to send price to given Telegram chat bot

--- cry_not/utilities.py
+++ cry_not/utilities.py
@@ -1,5 +1,5 @@
 import json
-from requests import Request, Session
+import requests
 
 
 def get_telegram_bot_token():
@@ -8,24 +8,27 @@ def get_telegram_bot_token():
         return data['telegramBotToken']
 
 
+def get_telegram_chat_id():
+    with open('config.json', 'r') as chat_id_file:
+        data = json.load(chat_id_file)
+        return data['chatId']
+
+
 def get_coinmarket_api_key():
     with open('config.json', 'r') as api_key_file:
         data = json.load(api_key_file)
         return data['coinmarketApiToken']
 
 
-def save_config(telegram_bot_token, coinmarket_api_key):
+def save_config(telegram_bot_token, telegram_chat_id, coinmarket_api_key):
     with open('config.json', 'w+') as bot_token_file:
         telegram_bot_string = f'\"telegramBotToken\": \"{telegram_bot_token}\"'
+        telegram_chat_id = f'\"chatId\": \"{telegram_chat_id}\"'
         coinmarket_api_string = f'\"coinmarketApiToken\": \"{coinmarket_api_key}\"'
-        json_string = '{' + telegram_bot_string + ',\n' + coinmarket_api_string + '}'
+        json_string = '{' + telegram_bot_string + ',\n' + telegram_chat_id + ', \n' + coinmarket_api_string + '}'
         bot_token_file.write(json_string)
 
 
-# TODO:
-#   1. Get coinmarket api key
-#   2. Insert api key into request header
-#   3. Make requests
 def get_prices(coinmarket_api_key):
     coinmarket_api_url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
 
@@ -39,9 +42,21 @@ def get_prices(coinmarket_api_key):
         'X-CMC_PRO_API_KEY': coinmarket_api_key,
     }
 
-    session = Session()
+    session = requests.Session()
     session.headers.update(headers)
 
     response = session.get(coinmarket_api_url, params=parameters)
     data = json.loads(response.text)
     return data['data']['1']['quote']['EUR']['price']
+
+
+def send_prices(telegram_bot_token, chat_id, price):
+    text = f'Bitcoin price: {price}'
+    telegram_api_url = f'https://api.telegram.org/bot{telegram_bot_token}/sendMessage'
+
+    parameters = {
+        'chat_id': chat_id,
+        'text': text
+    }
+
+    requests.get(telegram_api_url, params=parameters)
--- crypto_notifications.py
+++ crypto_notifications.py
@@ -2,19 +2,20 @@ import os
 import sys
 import requests
 from cry_not import utilities
-# https://api.telegram.org/bot<token>/METHOD_NAME
 
 
 def main():
     if 'config.json' not in os.listdir(os.getcwd()):
         telegram_bot_token = input('Please enter your Telegram bot token:\n')
+        telegram_chat_id = input('Please enter your Telegram chat ID:\n')
         coinmarket_api_key = input('Please enter your Coinmarket API key:\n')
-        utilities.save_config(telegram_bot_token, coinmarket_api_key)
-    # utilities.get_bot_token()
+        utilities.save_config(telegram_bot_token, telegram_chat_id, coinmarket_api_key)
 
     telegram_bot_token = utilities.get_telegram_bot_token()
     coinmarket_api_key = utilities.get_coinmarket_api_key()
+    telegram_chat_id = utilities.get_telegram_chat_id()
     bitcoin_price = utilities.get_prices(coinmarket_api_key)
+    utilities.send_prices(telegram_bot_token, telegram_chat_id, bitcoin_price)
 
 
 if __name__ == '__main__':
